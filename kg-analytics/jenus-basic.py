ec4d3e874dda17f56510f811b45e40d7b8332ca6
Bikash <bikashprakashdash@gmail.com>
2026-01-30 17:29:20 +0530

    jenus  dockers

--- kg-analytics/jenus-basic.py
+++ kg-analytics/jenus-basic.py
@@ -1,29 +1,11 @@
-from gremlin_python import statics
-from gremlin_python.structure.graph import Graph
-from gremlin_python.process.graph_traversal import __
-from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
-
 from gremlin_python.process.anonymous_traversal import traversal
 
-try:
-    connection = DriverRemoteConnection('ws://localhost:8182/gremlin', 'g')
-    g = traversal().withRemote(connection)
-    
-    user_node = g.addV('User').property('name', 'Alice').property('age', 30).next()
-    
-    
-    
-    # Query the data
-    print('\nUsers in graph:')
-    users = g.V().hasLabel('User').valueMap().toList()
-    print(users)
-    
-except Exception as e:
-    print(e)
-    print("Make sure Gremlin server is running on ws://localhost:8182/gremlin")
-    
-finally:
-    try:
-        connection.close()
-    except:
-        pass
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+
+g = traversal().withRemote(DriverRemoteConnection('ws://localhost:8182/gremlin','g'))
+
+g.addV('person').property('uid','bikash').property('name', 'Bikash Dash').iterate()
+g.addV('person').property('uid','puspa').property('name', 'Puspa Dash').iterate()
+g.addV('person').property('uid','pragyan').property('name', 'Pragyan Dash').iterate()
+
+print(g.V().hasLabel('person').valueMap().toList())
